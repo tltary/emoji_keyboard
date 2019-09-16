0d53ae92b37fb7b0ba2267cf4750fbb44f40b13d
Antony Pyatkov <tltary@yandex.ru>
2019-09-16 23:23:57 +0400

    add console.log after click emoji

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1034,6 +1034,7 @@ keyboard_button.addEventListener('click', function(e){
 			item.addEventListener('click',function(){
 				keyboard_area.value += this.dataset.name;
 				keyboard_area.focus()
+				console.log(keyboard_area.value)
 			},true)
 		})
 	}
